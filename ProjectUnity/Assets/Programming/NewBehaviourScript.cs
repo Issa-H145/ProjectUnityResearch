88190bb50be6dcab468a476b3364f65b31bb2fc1
Itha <Hamidehissa@outlook.com>
2024-06-11 17:19:50 -0400

    Another test
    you know the dealio

--- ProjectUnity/Assets/Programming/NewBehaviourScript.cs
+++ ProjectUnity/Assets/Programming/NewBehaviourScript.cs
@@ -8,7 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         transform.Translate(0, 0, 0);
-        
+        transform.Translate(0, 0, 0);
     }
 
     // Update is called once per frame
