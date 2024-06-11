fb14b4fd6d062bc9528a47e3e31b54e391b12b92
Itha <Hamidehissa@outlook.com>
2024-06-11 17:07:38 -0400

    Test Change

--- ProjectUnity/Assets/Programming/NewBehaviourScript.cs
+++ ProjectUnity/Assets/Programming/NewBehaviourScript.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.Translate(0, 0, 0);
         
     }
 
