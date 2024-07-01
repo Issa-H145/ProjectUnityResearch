3e54c779f432de92553854010b2ea36573190acf
Itha <Hamidehissa@outlook.com>
2024-07-01 11:17:08 -0400

    Code test changes on Parcel

--- ProjectUnity/Assets/Programming/Parcel.cs
+++ ProjectUnity/Assets/Programming/Parcel.cs
@@ -1,16 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityVector3 = UnityEngine.Vector3;
 using UnityEngine;
+using NumericsVector3 = System.Numerics.Vector3;
+using UnityEngine.UIElements;
 
-public class Parcel : MonoBehaviour
-{
+namespace parcal {
+
+public sealed class Parcel : MonoBehaviour{
+
+
+    public GameObject LargeBoxor; 
+
+    public GameObject ConveyerLong; /* where the LargeBox would go to and labels
+                                the truck as a GameObject to be accessible inside 
+                                the world. */
+    [SerializeField] float speed = 5f; /* The speed of how fast the box is going.
+                                            Once the box touches the conveyer via trigger
+                                            then it will start moving torwards its destination. Making this a 
+                                            SerializeField so we don't tinker the movement while accessing other
+                                            scripts and making it visible to the inspector. Makes it public to the
+                                            naked eye (*)v(*) but prevents further changes*/
+
+    public Rigidbody LargeBoxConnecter; /*Connecting LargeBoxConnector to the GetComponent<RigidBody>*/
+
     // Start is called before the first frame update
     void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update(){
+    // Update is called once per frame  
+
+    void Update() {
+
+    }
+
+    void OnCollisionEnter(Collision other) {
+
+    }
+
+
+
+    void OnTriggerEnter(Collider LargeBox) {
+
+        LargeBoxConnecter = LargeBox.GetComponent<Rigidbody>();
+
+                    
+                if(LargeBox.CompareTag("box-large") && LargeBoxConnecter != null){ 
+                    LargeBoxor.transform.position = Vector3.MoveTowards(LargeBoxor.transform.position, 
+                                                                        ConveyerLong.transform.position, 
+                                                                        speed * Time.deltaTime); 
+                }
+
         
     }
+    
+    
 }
+}
+ 
