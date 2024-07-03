b4fa754da3debb911904dcc4a05c170f3bc76dec
Itha <Hamidehissa@outlook.com>
2024-07-03 11:57:30 -0400

    Scenery Example

--- ProjectUnity/Assets/Programming/Eventrum.cs
+++ ProjectUnity/Assets/Programming/Eventrum.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Eventrum : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(0, 0, 0);
-        transform.Translate(0, 0, 0);
+        
     }
 
     // Update is called once per frame
