09a1c7d96cc667bc0d082c4dfe14ae7b677d85b0
Itha <Hamidehissa@outlook.com>
2024-07-11 17:56:47 -0400

    Comments on the code

--- ProjectUnity/Assets/Programming/Parcel.cs
+++ ProjectUnity/Assets/Programming/Parcel.cs
@@ -1,48 +1,86 @@
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class Parcel : MonoBehaviour{
-        List<GameObject> DifferentBoxes = new List<GameObject>();
-        public GameObject boxLong;
-        public GameObject boxWide;
-        public GameObject boxLarge;
-        public GameObject boxSmall;
-        //public GameObject anotherConveyor;
-        private Vector3 startingPosition;
+        List<GameObject> DifferentBoxes = new List<GameObject>(); /*A list that contains
+        different parcel GameObjects. This list will contain the GameObjects that will be'
+        be used throughout the Conveyor*/
+        public GameObject boxLong; //Connected to the box-long asset in the scene
+        public GameObject boxWide; //Connected to the box-wide asset in the scene
+        public GameObject boxLarge; //Connected to the box-large asset in the scene
+        public GameObject boxSmall; //Connected to the box--small asset in the scene
+        private Vector3 startingPosition; /*Declaring a specifc spawnPoint in the scene
+                                             meaning we want all the parcels to spawn at 
+                                             the loading area.*/
+        
 
 
     // Start is called before the first frame update
     void Start(){
 
-        DifferentBoxes.Add(boxLong);
-        DifferentBoxes.Add(boxWide);
-        DifferentBoxes.Add(boxLarge);
-        DifferentBoxes.Add(boxSmall);
+        DifferentBoxes.Add(boxLong); /*Once the scene starts, the parcels will immediately be 
+                                        added to the DifferentBoxes list/*/
+        DifferentBoxes.Add(boxWide);// <--|
+        DifferentBoxes.Add(boxLarge);// <-|
+        DifferentBoxes.Add(boxSmall);// <-|
     }
 
     // Update is called once per frame
     void Update(){
 
-            RandomBox();
+            RandomBox(); //Calling the RandomBox function to generate a parcel
     }
 
     private void RandomBox(){
 
-        int randomize = Random.Range(0, DifferentBoxes.Count);
-        GameObject ofSomeBox = DifferentBoxes[randomize];
-        ofSomeBox.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
-        SpawnBox(ofSomeBox);
+        int randomize = Random.Range(0, DifferentBoxes.Count); /*Making an int randomize by 
+                                                                ranging 0 to the size of the 
+                                                                list being 3*/
+        GameObject ofSomeBox = DifferentBoxes[randomize]; /*Once we have a randomized number from
+                                                            the range, we will use this as an element
+                                                            to get a specific parcel from the list. We're
+                                                            also declaring this as a Gameobject to ge the
+                                                            actual 3D asset.*/
+        ofSomeBox.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f); /*Scaling the parcel so it's fits inside 
+                                                                         inside the conveyor belt.*/
+
+        SpawnBox(ofSomeBox); /*Calling the SpawnBox function with a GameObject
+                                ofSomeBox.*/
 
     }
 
-     public void SpawnBox(GameObject BoxFromOther){
+     public void SpawnBox(GameObject BoxFromOther){ /*This initially was on a seperate 
+                                                        script but didn't think it was needed
+                                                        because of unecessary code*/
         
-        if(Input.GetKeyDown(KeyCode.Space)){
-        startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f));
-        Instantiate(BoxFromOther, startingPosition, Quaternion.identity);
+
+        if(Input.GetKeyDown(KeyCode.Space)){ //Pressing space makes the box spawn
+        startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels 
+                                                                                    should spawn in the Gameworld. */
+        Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+                                                                            BoxFromOther that was passed from tge SpawnBox function. 
+                                                                            We then use the startingPosition coordinates that we made earlier.
+                                                                            The last one that we made was was the Quaternion.identity to make
+                                                                            no rotation.*/
          }
     }
 
-  
 }
+
+/*The first idea of the parcel spawner was that we would have to create a seperate
+script that contained the ofSomeBox GameObject it would look like
+
+private Spawner ofSomeSpawn; <-- Calling the Spawner script
+
+ofSomeSpawn = GetComponent<Spawner>(); <-- Finding any object type in that script
+
+ofSomeSpawn.SpawnBox(ofSomeBox); <-- Calling a specific function in that script that we linked together.
+
+I decided to not include in a seperate script and only use it a parcel script.
+
+
+*/
