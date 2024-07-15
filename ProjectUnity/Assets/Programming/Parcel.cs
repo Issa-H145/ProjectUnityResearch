a951310787106b43c43f1d37abf71daf0aa1cb9d
Itha <Hamidehissa@outlook.com>
2024-07-15 10:02:43 -0400

    Zipcode tests

--- ProjectUnity/Assets/Programming/Parcel.cs
+++ ProjectUnity/Assets/Programming/Parcel.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using System.Threading;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 
 public class Parcel : MonoBehaviour{
+
+    
         List<GameObject> DifferentBoxes = new List<GameObject>(); /*A list that contains
         different parcel GameObjects. This list will contain the GameObjects that will be'
         be used throughout the Conveyor*/
+        
+        List<int> ZipCode = new List<int>();
+
         public GameObject boxLong; //Connected to the box-long asset in the scene
         public GameObject boxWide; //Connected to the box-wide asset in the scene
         public GameObject boxLarge; //Connected to the box-large asset in the scene
@@ -16,23 +22,32 @@ public class Parcel : MonoBehaviour{
         private Vector3 startingPosition; /*Declaring a specifc spawnPoint in the scene
                                              meaning we want all the parcels to spawn at 
                                              the loading area.*/
+                                                  
         
 
 
     // Start is called before the first frame update
-    void Start(){
+    private void Start(){
 
         DifferentBoxes.Add(boxLong); /*Once the scene starts, the parcels will immediately be 
                                         added to the DifferentBoxes list/*/
         DifferentBoxes.Add(boxWide);// <--|
         DifferentBoxes.Add(boxLarge);// <-|
         DifferentBoxes.Add(boxSmall);// <-|
+        ZipCode.Add(30190);
+        ZipCode.Add(46675);
+        ZipCode.Add(01075);
+        ZipCode.Add(24701);
+
     }
 
     // Update is called once per frame
     void Update(){
 
+           if(Input.GetKeyDown(KeyCode.Space)){ //Pressing space makes the box spawn
+
             RandomBox(); //Calling the RandomBox function to generate a parcel
+           }
     }
 
     private void RandomBox(){
@@ -56,19 +71,20 @@ public class Parcel : MonoBehaviour{
      public void SpawnBox(GameObject BoxFromOther){ /*This initially was on a seperate 
                                                         script but didn't think it was needed
                                                         because of unecessary code*/
-        
-
-        if(Input.GetKeyDown(KeyCode.Space)){ //Pressing space makes the box spawn
         startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels 
                                                                                     should spawn in the Gameworld. */
-        Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+        GameObject CreatingBox = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
                                                                             BoxFromOther that was passed from tge SpawnBox function. 
                                                                             We then use the startingPosition coordinates that we made earlier.
-                                                                            The last one that we made was was the Quaternion.identity to make
-                                                                            no rotation.*/
-         }
-    }
+                                                                            The last one that we made was was the Quaternion.identity to make no rotation.*/ 
+            
+         int randomZipIndex = Random.Range(0, ZipCode.Count);
+         int ActualZipCode = ZipCode[randomZipIndex];
+         
 
+
+
+     }
 }
 
 /*The first idea of the parcel spawner was that we would have to create a seperate
