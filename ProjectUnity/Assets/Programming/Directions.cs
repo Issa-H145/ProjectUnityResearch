a3f16453644df960c436a9221df7ded5daeb4e4a
Itha <Hamidehissa@outlook.com>
2024-07-18 09:26:08 -0400

    Parcel Changes and new assets

---
+++ ProjectUnity/Assets/Programming/Directions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Directions : MonoBehaviour{
+    
+    private Vector3 startingPosition;
+
+    public void Bin1(GameObject usingBox, int usingZipcode){
+        
+    }
+
+
+}
--- ProjectUnity/Assets/Programming/Parcel.cs
+++ ProjectUnity/Assets/Programming/Parcel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Threading;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
@@ -8,36 +9,60 @@ using UnityEngine.UIElements;
 
 public class Parcel : MonoBehaviour{
 
+    public struct BoxInfo{
+        public GameObject box;
+        public int zipcode;
     
-        List<GameObject> DifferentBoxes = new List<GameObject>(); /*A list that contains
+
+    public BoxInfo(GameObject boxing,  int zipper){
+
+        box = boxing;
+        zipcode = zipper;
+
+     }
+    }
+
+    public BoxInfo [] BoxSomething;
+
+    /*
+        public Dictionary<GameObject, int> DifferentBoxes = new Dictionary<GameObject, int>(); /*A list that contains
         different parcel GameObjects. This list will contain the GameObjects that will be'
         be used throughout the Conveyor*/
-        
-        List<int> ZipCode = new List<int>();
-
-        public GameObject boxLong; //Connected to the box-long asset in the scene
-        public GameObject boxWide; //Connected to the box-wide asset in the scene
-        public GameObject boxLarge; //Connected to the box-large asset in the scene
-        public GameObject boxSmall; //Connected to the box--small asset in the scene
+        public GameObject boxLarge; //Connected to the box-long asset in the scene
+        public GameObject Box_1;
+        public GameObject Box_2;
+        public GameObject Barrel;
         private Vector3 startingPosition; /*Declaring a specifc spawnPoint in the scene
                                              meaning we want all the parcels to spawn at 
                                              the loading area.*/
+
+        public Directions direct;
                                                   
         
 
 
     // Start is called before the first frame update
-    private void Start(){
+    void Start(){
 
-        DifferentBoxes.Add(boxLong); /*Once the scene starts, the parcels will immediately be 
-                                        added to the DifferentBoxes list/*/
-        DifferentBoxes.Add(boxWide);// <--|
-        DifferentBoxes.Add(boxLarge);// <-|
-        DifferentBoxes.Add(boxSmall);// <-|
-        ZipCode.Add(30190);
-        ZipCode.Add(46675);
-        ZipCode.Add(01075);
-        ZipCode.Add(24701);
+        BoxSomething = new BoxInfo[]{
+                new BoxInfo(boxLarge, 30190),
+                new BoxInfo(Barrel, 30190),
+                new BoxInfo(Box_1, 30190),
+                new BoxInfo(Box_2, 30190),
+                new BoxInfo(boxLarge, 46675),
+                new BoxInfo(Barrel, 46675),
+                new BoxInfo(Box_1, 46675),
+                new BoxInfo(Box_2, 46675),
+                new BoxInfo(boxLarge, 11075),
+                new BoxInfo(Barrel, 11075),
+                new BoxInfo(Box_1, 11075),
+                new BoxInfo(Box_2, 11075),
+                new BoxInfo(boxLarge, 24701),
+                new BoxInfo(Barrel, 24701),
+                new BoxInfo(Box_1, 24701),
+                new BoxInfo(Box_2, 24701)
+
+        };
 
     }
 
@@ -45,44 +70,69 @@ public class Parcel : MonoBehaviour{
     void Update(){
 
            if(Input.GetKeyDown(KeyCode.Space)){ //Pressing space makes the box spawn
-
             RandomBox(); //Calling the RandomBox function to generate a parcel
            }
+           
     }
 
     private void RandomBox(){
 
-        int randomize = Random.Range(0, DifferentBoxes.Count); /*Making an int randomize by 
+        int randomize = Random.Range(0, BoxSomething.Length); /*Making an int randomize by 
                                                                 ranging 0 to the size of the 
                                                                 list being 3*/
-        GameObject ofSomeBox = DifferentBoxes[randomize]; /*Once we have a randomized number from
+        GameObject ofSomeBox = BoxSomething[randomize].box; /*Once we have a randomized number from
                                                             the range, we will use this as an element
                                                             to get a specific parcel from the list. We're
                                                             also declaring this as a Gameobject to ge the
                                                             actual 3D asset.*/
+        int ofSomeZipCode = BoxSomething[randomize].zipcode;
         ofSomeBox.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f); /*Scaling the parcel so it's fits inside 
                                                                          inside the conveyor belt.*/
 
-        SpawnBox(ofSomeBox); /*Calling the SpawnBox function with a GameObject
+        SpawnBox(ofSomeBox, ofSomeZipCode); /*Calling the SpawnBox function with a GameObject
                                 ofSomeBox.*/
 
     }
 
-     public void SpawnBox(GameObject BoxFromOther){ /*This initially was on a seperate 
+     public void SpawnBox(GameObject BoxFromOther, int ZipCodeFromOther){ /*This initially was on a seperate 
                                                         script but didn't think it was needed
                                                         because of unecessary code*/
-        startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels 
-                                                                                    should spawn in the Gameworld. */
-        GameObject CreatingBox = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+        switch(ZipCodeFromOther){
+            case 30190:
+            Debug.Log($"{ZipCodeFromOther} and the box is {BoxFromOther}");
+            startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels                                                                should spawn in the Gameworld. */
+            GameObject boxoring = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
                                                                             BoxFromOther that was passed from tge SpawnBox function. 
                                                                             We then use the startingPosition coordinates that we made earlier.
-                                                                            The last one that we made was was the Quaternion.identity to make no rotation.*/ 
-            
-         int randomZipIndex = Random.Range(0, ZipCode.Count);
-         int ActualZipCode = ZipCode[randomZipIndex];
-         
-   
+                                                                          The last one that we made was was the Quaternion.identity to make no rotation.*/ 
+            break;
+            case 46675:
+             Debug.Log($"{ZipCodeFromOther} and the box is {BoxFromOther}");
+            startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels                                                                should spawn in the Gameworld. */
+            GameObject boxoring1 = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+                                                                            BoxFromOther that was passed from tge SpawnBox function. 
+                                                                            We then use the startingPosition coordinates that we made earlier.
+                                                                          The last one that we made was was the Quaternion.identity to make no rotation.*/ 
+            break;
+            case 11075:
+            Debug.Log($"{ZipCodeFromOther} and the box is {BoxFromOther}");
+            startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels                                                                should spawn in the Gameworld. */
+            GameObject boxoring2 = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+                                                                            BoxFromOther that was passed from tge SpawnBox function. 
+                                                                            We then use the startingPosition coordinates that we made earlier.
+                                                                          The last one that we made was was the Quaternion.identity to make no rotation.*/ 
 
+            break;
+            case 24701:
+            Debug.Log($"{ZipCodeFromOther} and the box is {BoxFromOther}");
+            startingPosition = new Vector3(-2.43f, 0.87f, Random.Range(-0.2f, 1.9f)); /*Making specific coordinates where the parcels                                                                should spawn in the Gameworld. */
+            GameObject something = Instantiate(BoxFromOther, startingPosition, Quaternion.identity); /*Creating randomized clones of parcels using the 
+                                                                            BoxFromOther that was passed from tge SpawnBox function. 
+                                                                            We then use the startingPosition coordinates that we made earlier.
+                                                                          The last one that we made was was the Quaternion.identity to make no rotation.*/ 
+            break;
+
+        }
 
      }
 }
